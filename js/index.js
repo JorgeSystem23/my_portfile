01be84ef193f9fb6c28e0a07426c500317e2dfb0
JorgeSystem23 <prosystem155@gmail.com>
2024-03-22 09:56:43 -0600

    portfolio color configuration

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // JavaScript para el proyecto HTML
 
 // Función para seleccionar un enlace del menú
-function seleccionar() {
+function select() {
   // Aquí puedes agregar el código para manejar la selección del enlace del menú
   // Por ejemplo, podrías resaltar el enlace seleccionado o desplazarte suavemente a la sección correspondiente
   // Aquí te muestro un ejemplo de desplazamiento suave hacia la sección correspondiente
@@ -11,15 +11,29 @@ function seleccionar() {
 }
 
 // Función para ocultar el menú en dispositivos móviles
-function ocultarMenu() {
+function hideMenu() {
   // Aquí puedes agregar el código para ocultar el menú cuando se hace clic en el ícono de menú en dispositivos móviles
   // Por ejemplo, podrías ocultar el menú al hacer clic en el ícono de menú
   const nav = document.getElementById("nav"); // Seleccionar el menú de navegación
   nav.classList.toggle("activo"); // Alternar la clase 'activo' para ocultar o mostrar el menú
 }
 
+// Función para ir al principio de la página
+function goToBeginning() {
+  window.scrollTo(0, 0);
+}
+
+// Agregar la función al evento `click` del botón de actualizar
+document
+  .querySelector(".boton-actualizar")
+  .addEventListener("click", goToBeginning);
+
 // Event Listener para cargar el contenido de la página
 document.addEventListener("DOMContentLoaded", function () {
   // Aquí puedes agregar el código que necesitas ejecutar cuando la página se carga
   // Por ejemplo, podrías inicializar ciertos componentes o realizar alguna acción específica
+  // Agregar la función al evento `click` del botón de actualizar
+  document
+    .querySelector(".boton-actualizar")
+    .addEventListener("click", goToBeginning);
 });
